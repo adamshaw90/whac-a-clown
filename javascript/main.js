9fcbfa0059acae615fba7c15d167017e006fe676
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-09-06 22:38:51 +0000

    add css styling to grid, add javascript for grid/box images

--- javascript/main.js
+++ javascript/main.js
@@ -5,4 +5,9 @@ for (let i = 1; i <= 16; i++)
     let box = document.createElement("div")
     box.classList.add("box")
     gameboarddiv.appendChild(box);
+    
+    let pipe = document.createElement("img")
+    pipe.classList.add("pipe")
+    pipe.src = "assets/images/drain.png"
+    box.appendChild(pipe);
 }
